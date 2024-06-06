b549d19d77c6383d32413f8e6f294c48a9e59349
Roman Vogman <romanvogman@Romans-MacBook-Pro.local>
2024-06-06 22:12:58 +0300

    clone repo to dedicated path

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,5 +6,5 @@ sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
-git clone https://github.com/romanvog/dream.git
+git clone https://github.com/romanvog/dream.git /home/ubuntu/dream
 sudo docker-compose -f dream/compose.yaml up
